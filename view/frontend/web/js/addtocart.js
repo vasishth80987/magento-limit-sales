6d3a846cb1b667365fc9a7a4ffa19fd9b88d5d65
Vash <vash@vsynchdigitalsolutions.com.au>
2020-06-03 07:21:03 +1000

    Fixed Add to Cart for configurable products

--- view/frontend/web/js/addtocart.js
+++ view/frontend/web/js/addtocart.js
@@ -19,7 +19,10 @@ define([
             });
             $('#product-addtocart-button').click(function(e){
                 var qty_selected = parseInt($('#qty').val());
-                console.log(cart_items,qty_selected,qty_allowed,qty_selected+cart_items>qty_allowed);
+                var swatches = $(".swatch-attribute");
+                var swatch_selected = $(".swatch-attribute").map(function(){return $(this).attr("option-selected");}).get();
+                if(swatch_selected.length!=swatches.length) return;
+                console.log(cart_items,qty_selected,qty_allowed,qty_selected+cart_items>qty_allowed,swatch_selected,swatches);
                 if(qty_selected+cart_items>qty_allowed){
                     $('#LimitSalesWarning2').show();
                     setInterval(function () {
